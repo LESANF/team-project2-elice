657917e5197c0970ca9c08b57fd204c232bdc9a5
LESANF <nagong1000@naver.com>
2022-12-21 23:10:07 +0900

    ♻ refactor: add maps, photolists path

--- src/Router.tsx
+++ src/Router.tsx
@@ -9,6 +9,7 @@ import Maps from './Pages/Maps';
 import Join from './Pages/Join/Page';
 import Edit from './Pages/Edit/Page';
 import MyPage from './Pages/MyPage';
+import ErrorPage from './Components/Commons/ErrorPage';
 
 const Router = () => (
   <BrowserRouter>
@@ -23,11 +24,11 @@ const Router = () => (
       />
       <Route path="/menu/maps" element={<Menu menuType="map" />} />
       <Route path="/menu/photolists" element={<Menu menuType="photo" />} />
-      <Route path="/maps" element={<Maps />} />
       <Route path="/join" element={<Join />} />
       <Route path="/edit" element={<Edit />} />
       <Route path="/mypage" element={<MyPage />} />
       <Route path="/test" element={<Test />} />
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   </BrowserRouter>
 );
