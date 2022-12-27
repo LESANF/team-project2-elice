d03cd9f91e29cc255c1be607ba8a8a034f14f135
채현 김 <75241542+ggongjukim@users.noreply.github.com>
2022-12-28 03:50:00 +0900

    ✨ feat: mypage tap 시작

--- src/Pages/MyPage/Components/MypostTap/index.tsx
+++ src/Pages/MyPage/Components/MypostTap/index.tsx
@@ -1,5 +1,12 @@
-import * as S from './styled';
+import * as P from '../../../PhotoLists/Page/styled';
+import { ImageList } from '@mui/material';
 
-const MypostTap = () => <div style={{ backgroundColor: 'red' }}>나의 사진</div>;
+const MypostTap = () => {
+  return (
+    <P.Container>
+      {/* <ImageList variant="masonry" cols={4} gap={16}></ImageList> */}
+    </P.Container>
+  );
+};
 
 export default MypostTap;
